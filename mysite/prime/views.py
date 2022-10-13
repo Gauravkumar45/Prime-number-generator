bd6bd0f52dc77193a6968de6c3af81ed90044e82
gaurav kumar <gk4454543@gmail.com>
2022-10-13 16:56:37 +0530

    first commit

---
+++ mysite/prime/views.py
@@ -0,0 +1,9 @@
+from urllib import request
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,'index.html')
+    
+    
+    
